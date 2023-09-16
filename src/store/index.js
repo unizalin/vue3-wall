6b8a54836f2b36826122f017cd8288f89d411468
mratyao <abccc861107@gmail.com>
2023-09-16 18:20:21 +0800

    Updates

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'vuex'
-import user from '@/store/modules/user'
-import ui from '@/store/modules/ui'
-import post from '@/store/modules/post'
-
-export default createStore({
-  modules: { user, ui, post }
-})
